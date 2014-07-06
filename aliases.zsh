0e3f59a002b0a38bf8987f323caee4623acf11f4
Tyler <tvtran2@gmail.com>
2014-07-06 16:53:50 +0000

    initial commit

---
+++ aliases.zsh
@@ -0,0 +1,35 @@
+# Push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
+
+# Basic directory operations
+alias ...='cd ../..'
+alias -- -='cd -'
+
+# Super user
+alias _='sudo'
+alias please='sudo'
+
+#alias g='grep -in'
+
+# Show history
+if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]
+then
+    alias history='fc -fl 1'
+elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]
+then
+    alias history='fc -El 1'
+elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ]
+then
+    alias history='fc -il 1'
+else
+    alias history='fc -l 1'
+fi
+# List direcory contents
+alias ll='ls -lah'
+alias l='ls -lah'
+alias lh='ls -lh'
+alias la='ls -lAh'
+alias sl=ls # often screw this up
+
+alias afind='ack-grep -il'
