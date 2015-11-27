de58750188038a1efadd02d9baac3b00d8503c1f
Tyler Tran <tvtran2@gmail.com>
2015-11-26 21:00:27 -0600

    Add aliases for real

---
+++ aliases.zsh
@@ -0,0 +1,3 @@
+alias 330="ssh tyler@ec2-54-187-233-19.us-west-2.compute.amazonaws.com"
+alias 330root="ssh -i ~/.ssh/default.pem ubuntu@ec2-54-187-233-19.us-west-2.compute.amazonaws.com"
+alias 330sftp="sftp tyler@ec2-54-187-233-19.us-west-2.compute.amazonaws.com"
